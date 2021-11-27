8ea4061b2591ca9288f9d296f1880ad1c6f0ba80
Seoin <befine324@gmail.com>
2021-11-27 09:26:00 +0900

    first commit

---
+++ src/main/java/dev/cal/DevCalApplication.java
@@ -0,0 +1,13 @@
+package dev.cal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cal/DevCalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
